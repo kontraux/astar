b618c82b208094791bef39534029f14b1456f0eb
Sam <samuelblake@gmail.com>
2022-09-18 11:58:14 -0500

    added the basic functionality

--- astar.lua
+++ astar.lua
@@ -1,29 +1,103 @@
 local astar = {}
 
-function astar.new(startx, starty, endx, endy)
-    local open = {
-        { startx, starty }
-    }
-    local closed = {}
-    local f -- Total cost of path to n
-    local g -- Cost so far
-    local h -- Cost from n to goal (heuristic)
-    while startx ~= endx and starty ~= endy  do
-        local n = table.remove(open, 1)
-        table.insert(closed, n)
-        print(n[1], n[2])
-        for i = -1, 1 do
-            for j = -1, 1 do
-                for i,v in pairs(closed) do
-                    if i ~= v[1] and j ~= v[2] then
+function astar.route(start_node, end_node, map)
+local open = {}
+local closed = {}
 
+local first_node = {
+    x = start_node.x,
+    y = start_node.y,
+    h = 0,
+    g = 0,
+    f = 0,
+}
+
+table.insert(open, first_node)
+local current_node
+
+    local function new_node(x, y)
+        local node = {
+            x = x,
+            y = y,
+        }
+        node.h = node.x - end_node.x + node.y - end_node.y
+        node.g = current_node.g + 1
+        node.f = node.g + node.h
+        return node
+    end
+
+    local function get_neighbors(node)
+        local neighbors = {}
+        for x = node.x - 1, node.x + 1 do
+            for y = node.y - 1, node.y + 1 do
+                if x ~= node.x or y ~= node.y then
+                    if x > 0 and y > 0 and x < #map and y < #map[x] then
+                        if map[x][y] == 0 then
+                            local neighbor_node = new_node(x, y)
+                            table.insert(neighbors, neighbor_node)
+                        end
                     end
                 end
             end
         end
+        return neighbors
     end
-end
 
+    local function in_table(node, table)
+        local value = false
+        for i, v in pairs(table) do
+            if node.x == v.x and node.y == v.y then
+                value = true
+            end
+        end
+        return value
+    end
+
+    local function reconstruct_path(node)
+        local path = {}
+        while node.parent.x ~= start_node.x and node.parent.y ~= start_node.y do
+            table.insert( path, { x = node.parent.x, y = node.parent.y } )
+            for i,v in pairs(closed) do
+                if node.parent.x == v.x and node.parent.y == v.y then
+                    node = v
+                end
+            end
+        end
+        return path
+    end
+
+    local path = {}
+    local function begin()
+        ::continue::
+        current_node = table.remove(open, 1)
+
+        if current_node.x == end_node.x and current_node.y == end_node.y then
+            print("Found a path from ", start_node.x, start_node.y, "to ", end_node.x, end_node.y)
+            path = reconstruct_path(current_node)
+            return path
+        end
+
+        if in_table(current_node, closed) then
+            goto continue
+        end
+        table.insert(closed, current_node)
+
+        local neighbors = get_neighbors(current_node)
+        for _, node in pairs(neighbors) do
+            node.parent = {
+                x = current_node.x,
+                y = current_node.y,
+            }
+            if not in_table(node, closed) then
+                table.insert(open, node)
+            end
+        end
+        goto continue
+        return path
+    end
+    begin()
+    return path
+end
 
 
 return astar
--- map.lua
+++ map.lua
@@ -1,25 +1,71 @@
 local map = {}
 
-function map.create(rows, columns)
+
+function map:new(row, column)
     local grid = {}
-    for x = 1, rows do
+    setmetatable(grid, self)
+    self.__index = self
+    for x = 1, row do
         grid[x] = {}
-        for y = 1, columns do
+        for y = 1, column do
             grid[x][y] = 0
         end
     end
     return grid
 end
 
-function map.noise(grid, amount)
-    for x = 1, #grid do
-        for y = 1, #grid[x] do
-            if math.random() < amount then
-                grid[x][y] = 1
+function map:add_noise(percent)
+    for x = 1, #self do
+        for y = 1, #self[x] do
+            if math.random() < percent * 0.01 then
+                self[x][y] = 1
             end
         end
     end
 end
 
+function map:random_nodes()
+    ::retry::
+    local start_node = {
+        x = math.random(1, #self)
+    }
+    start_node.y = math.random(1, #self[start_node.x])
+    local end_node = {
+        x = math.random(1, #self)
+    }
+    end_node.y = math.random(1, #self[end_node.x])
+
+    if self[start_node.x][start_node.y] == 0 and self[end_node.x][end_node.y] == 0 then
+        return start_node, end_node
+    else
+        goto retry
+    end
+end
+
+function map:wall()
+    for x = 1, #self do
+        for y = 1, #self[x] do
+            if x == 1 or y == 1 or x == #self or y == #self[x] then
+                self[x][y] = 1
+            end
+        end
+    end
+    return self
+end
+
+function map:write_path(path)
+    for _, node in pairs(path) do
+        self[node.x][node.y] = " "
+    end
+end
+
+function map:print()
+    for _,t in pairs(self) do 
+        io.write("\n")
+        for i, v in pairs(t) do
+            io.write(v)
+        end
+    end
+end
 
 return map
